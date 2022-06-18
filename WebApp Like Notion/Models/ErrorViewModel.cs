4fbf4a7a1a3de2de4d151332b9d48cd28363ed5e
Name <empty>
2022-06-18 07:51:14 +0300

    Create project

---
+++ WebApp Like Notion/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace WebApp_Like_Notion.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
